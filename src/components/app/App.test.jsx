63e32f8250fcc3d5ea5ca49e4894879671d023e7
Austin Summerlin <summerlin.austin@gmail.com>
2021-08-03 10:58:15 -0700

    test

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
-import App from './App';
+import SeinfeldQuote from '../../containers/SeinfeldQuote';
 
 describe('App component', () => {
   afterEach(() => cleanup());
   it('renders App', () => {
-    const { asFragment } = render(<App />);
+    const { asFragment } = render(<SeinfeldQuote />);
     expect(asFragment()).toMatchSnapshot();
   });
 });
